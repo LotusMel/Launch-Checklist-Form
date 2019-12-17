858d6ef0d42e39d308393c6f15af827f03615117
Melva Lang <mjlang74@gmail.com>
2019-12-16 22:59:29 -0600

    took out load event

--- script.js
+++ script.js
@@ -1,59 +1,77 @@
 // Write your JavaScript code here!
-window.addEventListener("load", function () {
-   fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+fetch("https://handlers.education.launchcode.org/static/planets.json").then(
+   function (response) {
       response.json().then(function (json) {
          const div = document.getElementById("missionTarget");
-         let form = document.querySelector("form");
-         form.addEventListener("submit", function (event) {
-            //alert("All fields required!");
-            let pilotName = document.querySelector("input[name=pilotName]");
-            let copilotName = document.querySelector("input[name=copilotName]");
-            let fuelLevel = document.querySelector("input[name=fuelLevel]");
-            let cargoMass = document.querySelector("input[name=cargoMass]");
-            if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
-               alert("All fields required!");
-               event.preventDefault();
-            }
-         });
-      });
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
+         //let index = 2;
+         let planet = Math.floor(Math.random() * json.length);
+
+         div.innerHTML = `
+         <h2>Mission Destination</h2>
+         <ol>
+         <li>Name: ${json[planet].name}</li>
+         <li>Diameter: ${json[planet].diameter}</li>
+         <li>Star: ${json[planet].star}</li>
+         <li>Distance from Earth: ${json[planet].distance}</li>
+         <li>Number of Moons: ${json[planet].moons}</li>
+         </ol>
+         <img src="${json[planet].image}"/>
+         `;
+
+
+
+      let form = document.querySelector("form");
+      form.addEventListener("submit", function (event) {
+         //alert("All fields required!");
+         //event.preventDefault();
+         let pilotName = document.querySelector("input[name=pilotName]");
+         let copilotName = document.querySelector("input[name=copilotName]");
+         let fuelLevel = document.querySelector("input[name=fuelLevel]");
+         let cargoMass = document.querySelector("input[name=cargoMass]");
+
+         if (!pilotName || !copilotName || !fuelLevel || !cargoMass) {
+            alert("All fields required!");
+            event.preventDefault(submit);
+         };
+      /*
+This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
+<li>Name: ${}</li>
+<li>Diameter: ${}</li>
+<li>Star: ${}</li>
+<li>Distance from Earth: ${}</li>
+<li>Number of Moons: ${}</li>
 </ol>
 <img src="${}">
 
 window.addEventListener("load", function () {
 
-    let json = [];
-    let index = 0;
+let json = [];
+let index = 0;
 
-    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
-        response.json().then(function (json) {
-            const div = document.getElementById("container");
-            for(index = 0; index < json.length; index++){
+fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+  response.json().then(function (json) {
+      const div = document.getElementById("container");
+      for(index = 0; index < json.length; index++){
 
 
-                div.innerHTML += `
-                <div class="astronaut">
-                <div class="bio">
-                <h3>${json[index].firstName} ${json[index].lastName}</h3>
-                <ul>
-                <li>Hours in space: ${json[index].hoursInSpace}</li>
-                <li>Active: ${json[index].active}</li>
-                <li>Skills: ${json[index].skills}</li>
-                </ul>
-                </div>
-                <img class="avatar" src="${json[index].picture}">
-                </div>
-                `;
-            }
-        });
-    });
+          div.innerHTML += `
+          <div class="astronaut">
+          <div class="bio">
+          <h3>${json[index].firstName} ${json[index].lastName}</h3>
+          <ul>
+          <li>Hours in space: ${json[index].hoursInSpace}</li>
+          <li>Active: ${json[index].active}</li>
+          <li>Skills: ${json[index].skills}</li>
+          </ul>
+          </div>
+          <img class="avatar" src="${json[index].picture}">
+          </div>
+          `;
+      }
+  });
+});
 });
 
 */
